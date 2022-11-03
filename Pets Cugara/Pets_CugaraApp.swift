ff5da4376edb6735919ac15141bbc4d6938408ee
Murilo Rodrigues Maia <murilo.maia62@aluno.ifce.edu.br>
2022-11-03 14:46:37 -0300

    Initial Commit

---
+++ Pets Cugara/Pets_CugaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pets_CugaraApp.swift
+//  Pets Cugara
+//
+//  Created by Murilo Rodrigues Maia on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Pets_CugaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
